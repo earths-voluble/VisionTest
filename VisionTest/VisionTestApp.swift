cd009527411b13808747d79aebf274b54d49c38a
Paul Lee <earths-voluble.0g@icloud.com>
2023-06-22 06:45:33 +0900

    Initial Commit

---
+++ VisionTest/VisionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTestApp.swift
+//  VisionTest
+//
+//  Created by 이보한 on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
